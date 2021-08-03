eeda33a740395c465b52acb63129adbc79bfd812
kenshortrede <88390179+kenshortrede@users.noreply.github.com>
2021-08-03 10:52:37 -0400

    Update BitmapUtil.java

--- android/src/main/java/com/tablemi/flutter_sunmi_printer/utils/BitmapUtil.java
+++ android/src/main/java/com/tablemi/flutter_sunmi_printer/utils/BitmapUtil.java
@@ -17,15 +17,7 @@ import java.util.Hashtable;
 import java.util.Map;
 
 public class BitmapUtil {
-    /**
-     * 生成条码bitmap
-     *
-     * @param content
-     * @param format
-     * @param width
-     * @param height
-     * @return
-     */
+    
     public static Bitmap generateBitmap(String content, int format, int width, int height) {
         if (content == null || content.equals(""))
             return null;
@@ -104,28 +96,20 @@ public class BitmapUtil {
     }
 
 
-    /**
-     * 生成二维码 要转换的地址或字符串,可以是中文
-     *
-     * @param url
-     * @param width
-     * @param height
-     * @return
-     */
+    
     public static Bitmap createQRImage(String url, final int width, final int height) {
         try {
-            // 判断URL合法性
+            
             if (url == null || "".equals(url) || url.length() < 1) {
                 return null;
             }
             Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
             hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-            // 图像数据转换，使用了矩阵转换
+            
             BitMatrix bitMatrix = new QRCodeWriter().encode(url,
                     BarcodeFormat.QR_CODE, width, height, hints);
             int[] pixels = new int[width * height];
-            // 下面这里按照二维码的算法，逐个生成二维码的图片，
-            // 两个for循环是图片横列扫描的结果
+            
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     if (bitMatrix.get(x, y)) {
@@ -135,7 +119,7 @@ public class BitmapUtil {
                     }
                 }
             }
-            // 生成二维码图片的格式，使用ARGB_8888
+            
             Bitmap bitmap = Bitmap.createBitmap(width, height,
                     Bitmap.Config.ARGB_8888);
             bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
@@ -146,19 +130,17 @@ public class BitmapUtil {
         return null;
     }
 
-    /**
-     * 检查图片是否超过一定值，是则缩小
-     */
+    
     public static Bitmap convertToThumb(byte[] buffer, float size)
     {
-        // 获取原图宽度
+        
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         options.inPurgeable = true;
         options.inInputShareable = true;
         Bitmap bm = BitmapFactory.decodeByteArray(buffer, 0, buffer.length,
                 options);
-        // 计算缩放比例
+       
         float reSize = options.outWidth / size;
         if (options.outWidth > options.outHeight)
         {
@@ -169,7 +151,7 @@ public class BitmapUtil {
             reSize = 1;
         }
         Log.d("bitmap", "convertToThumb, reSize:" + reSize);
-        // 缩放
+        
         options.inJustDecodeBounds = false;
         options.inSampleSize = (int) reSize;
         if (bm != null && !bm.isRecycled())
